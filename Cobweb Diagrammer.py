be58fe2023a84daea09ad84c983534dde8d1e847
csimpson1 <csimpson.2@gmail.com>
2016-02-03 21:25:12 -0500

    Added a GUI so as to better accept user input

--- Cobweb Diagrammer.py
+++ Cobweb Diagrammer.py
@@ -1,8 +1,11 @@
-import turtle
+"""
+Written by Colby Simpson
+A cobweb diagram maker for investigating the behaviour of the logistic map
+"""
 
-#2Do
-# 1) Set up a GUI to accept user inputs for the initial value of iteration, and the growth parameter
-# 2) Add support for more than just the logistic map
+
+import Tkinter as tk
+import turtle
 
 
 def logistic_map(r, x_range):
@@ -32,67 +35,79 @@ def linspace(lower, upper, num_steps):
     return [lower + i*step_size for i in range(num_steps)]
 
 
-if __name__ == '__main__':
-    #master = Tk()
-    #canvas_width = 960
-    #canvas_height = 960
-    #window = Canvas(master, width = canvas_width, height = canvas_height)
-    #window.pack()
-    #woodland = turtle.TurtleScreen(window)
-    woodland = turtle.Screen()
-    woodland.setworldcoordinates(0, 0, 1, 1)
-    franklin = turtle.Turtle()
-    # Forget tying your shoes Franklin, its time to go fast
-    franklin.speed(0)
-
-
-    # This will be the seed for the iteration of the cobweb plotter
-    initial_val = 0.25
-    r = 3.2
-
-
-
-    # Draw x and y axes
-    franklin.setx( woodland.window_width() )
-    franklin.setx(0)
-    franklin.sety( woodland.window_height() )
-
+def cobweb(turt, r, num_it, initial_value):
+    # Clear the screen
+    turt.clear()
+    turt.speed(0)
     # Draw the line y = x in blue
-    franklin.penup()
-    franklin.goto(0,0)
-    franklin.color('blue')
-    franklin.pendown()
-    franklin.goto(1, 1)
+    turt.penup()
+    turt.goto(0,0)
+    turt.color('blue')
+    turt.pendown()
+    turt.goto(1, 1)
 
     # Draw the logistic map
-    franklin.penup()
-    franklin.goto(0,0)
-    franklin.pendown()
-    franklin.color('purple')
+    turt.penup()
+    turt.goto(0,0)
+    turt.pendown()
+    turt.color('purple')
 
     # Draw the polynomial map, for the time being, the logistic map
     turtle_x_range = linspace(0, 1, 100)
     turtle_y_range = logistic_map(r, turtle_x_range)
 
     for i in range( len( turtle_x_range ) ):
-        franklin.goto( turtle_x_range[i], turtle_y_range[i] )
+        turt.goto( turtle_x_range[i], turtle_y_range[i] )
 
     # Begin iteration of the map from our seed value
-    franklin.penup()
-    x_current = initial_val
-    franklin.goto(x_current, 0)
-    franklin.color('red')
-    franklin.pendown()
+    turt.penup()
+    x_current = initial_value
+    turt.goto(x_current, 0)
+    turt.color('red')
+    turt.pendown()
     # i think it would be cool to see the cobweb being drawn
-    franklin.speed(5)
+    turt.speed(5)
 
-    for i in range(200):
+    for i in range(num_it):
         x_new = logistic_map( r, x_current )
-        franklin.goto( x_current, x_new )
-        franklin.goto( x_new, x_new )
+        turt.goto( x_current, x_new )
+        turt.goto( x_new, x_new )
         x_current = x_new
 
+root = tk.Tk()  # Defines the main application window
+root.title('Cobweb Plotter')  # Puts a title along the top bar of the window
 
-    # to keep the  window from closing immediately
-    master.mainloop()
+r = tk.DoubleVar()
+num_it = tk.IntVar()
+initial_value = tk.DoubleVar()
 
+# Entry boxes for the r and num_it parameters
+growth_entry = tk.Entry(root, textvariable = r, bd = 5)
+growth_entry.grid(row=0, column=1)
+iter_entry = tk.Entry(root, textvariable = num_it, bd = 5)
+iter_entry.grid(row=0, column=3)
+init_entry = tk.Entry(root, textvariable = initial_value, bd = 5)
+init_entry.grid(row=0, column=5)
+
+# Labels for entry boxes
+growth_label = tk.Label(root, text = 'Growth Parameter')
+growth_label.grid(row=0, column=0)
+iter_label = tk.Label(root, text = 'Number of Iterations')
+iter_label.grid(row = 0, column=2)
+init_label = tk.Label(root, text = 'Initial Value')
+init_label.grid(row=0, column=4)
+
+# Where the magic happens
+woodland = tk.Canvas(width = 860, height = 860, bg = 'white')
+woodland.grid(row=1, columnspan = 7)
+
+franklin = turtle.RawTurtle(woodland)
+screen = franklin.getscreen()
+screen.setworldcoordinates(0,0,1,1)
+
+# Releases the spiders
+b = tk.Button(root, text = "Let's Go", command = lambda: cobweb(franklin, r.get(), num_it.get(), initial_value.get()))
+# The lambda: makes it so the cobweb function is only evaluated once the button is pressed
+b.grid(row=0, column=7)
+
+root.mainloop()
